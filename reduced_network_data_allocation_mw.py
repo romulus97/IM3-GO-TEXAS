8e46a1140cc6e4f5d100202e4cce3b916a4e955e
Henry Ssembatya <sembahen2@yahoo.com>
2024-02-07 17:52:04 -0500

    folders

--- reduced_network_data_allocation_mw.py
+++ reduced_network_data_allocation_mw.py
@@ -18,14 +18,14 @@ from pathlib import Path
 
 df_load = pd.read_csv('BA_load.csv',header=0)
 
-#NODE_NUMBER = NODE_NUMBER = [50,75,100,125,150,175,200,225,250,275,300]
+NODE_NUMBER = NODE_NUMBER = [50,75,100,125,150,175,200,225,250,275,300]
 #NODE_NUMBER = [50]
-NODE_NUMBER = [150]
+#NODE_NUMBER = [150]
 
 #UC_TREATMENTS = ['_coal']
 #UC_TREATMENTS = ['_coal', '_simple']
-UC_TREATMENTS = ['_coal_gas']
-#UC_TREATMENTS = ['_simple']
+#UC_TREATMENTS = ['_coal_gas']
+UC_TREATMENTS = ['_simple']
 
 trans_p = [0]
 #trans_p = [250, 500, 750, 1000, 1500]
